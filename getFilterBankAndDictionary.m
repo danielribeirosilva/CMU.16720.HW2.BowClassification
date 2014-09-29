f4e96504f1b352accca97936185a23107e88c5ca
Daniel <danielsilva.ita@gmail.com>
2014-09-28 20:21:59 -0400

    dictionary and visual word scripts

--- getFilterBankAndDictionary.m
+++ getFilterBankAndDictionary.m
@@ -1,5 +1,42 @@
-function [filterBank,dictionary] = getFilterBankAndDictionary(trainFiles)
+function [filterBank, dictionary] = getFilterBankAndDictionary(imPaths)
 
-%%%YOUR CODE HERE
+%parameters
+K = 150;
+alpha = 100;
+
+%filter bank
+filterBank = createFilterBank();
+
+%size parameters
+T = size(imPaths,1);
+N = 3 * size(filterBank,1);
+
+%complete set of filter responses
+allFilterResponses = zeros(alpha*T, N);
+
+for i = 1:length(imPaths)
+    
+    disp(i);
+   
+    %read image
+    I = imread(imPaths{i});
+    
+    %compute filter responses
+    imgResponses = extractFilterResponses(I,filterBank);
+    
+    %select alpha responses
+    selectedRows = randperm(size(imgResponses,1),alpha);
+    selectedResponses = imgResponses(selectedRows,:);
+    
+    %add to list of responses
+    startIdx = (i-1)*alpha + 1;
+    endIdx = startIdx + alpha - 1;
+    allFilterResponses( startIdx:endIdx, : ) = selectedResponses;
+    
+end
+
+%do k-means
+[unused, dictionary] = kmeans(allFilterResponses, K, 'EmptyAction', 'drop');
+
 
 end
--- getVisualWords.m
+++ getVisualWords.m
@@ -1,5 +1,16 @@
-function [wordMat]=getVisualWords(I,filterBank,dictionary)
-  %%%YOUR CODE HERE 
+function [wordMap]=getVisualWords(I,filterBank,dictionary)
+  
+%compute filter responses
+imgResponses = extractFilterResponses(I,filterBank);
+
+%compute distance between each point and each cluster center
+distances = pdist2(imgResponses,dictionary);
+
+%get index of lowest distance
+[minDist,index] = min(distances,[],2);
+
+%word map
+wordMap = vec2mat(index,size(I,2));
     
 end
 
