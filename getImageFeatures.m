4dc04a4ae8604c6e55aef9c0a768042ffc3aee17
Daniel <danielsilva.ita@gmail.com>
2014-09-29 01:08:05 -0400

    histogram of features

--- getImageFeatures.m
+++ getImageFeatures.m
@@ -1,3 +1,14 @@
 function [h] = getImageFeatures(wordMap,dictionarySize)
 
-%%%YOUR CODE HERE
+%count cluster occurences
+[count,clusters]=hist(wordMap,unique(wordMap));
+count = sum(count,2);
+
+%set 0 for remaining clusters and adjust order
+h = zeros(1,dictionarySize);
+h(clusters) = count;
+
+%L1 normalization
+h = h / numel(wordMap);
+
+end
